31fa865a0431f8c33f4b0cd5c3f22ca86908fe57
Yury <chilikov.yuri777@mail.ru>
2017-11-10 19:27:56 +0300

    Lesson 3 (full)

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -50,11 +50,11 @@ fun isPerfect(n: Int): Boolean {
  * Найти число вхождений цифры m в число n
  */
 fun digitCountInNumber(n: Int, m: Int): Int =
-        when {
-            n == m -> 1
-            n < 10 -> 0
-            else -> digitCountInNumber(n / 10, m) + digitCountInNumber(n % 10, m)
-        }
+    when {
+          n == m -> 1
+          n < 10 -> 0
+          else -> digitCountInNumber(n / 10, m) + digitCountInNumber(n % 10, m)
+    }
 
 /**
  * Тривиальная
@@ -64,11 +64,11 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  */
 fun digitNumber(n: Int): Int {
     var sum = 0
-    var N = n
+    var current = n
     do {
-        sum = sum + 1
-        N = N / 10
-    } while (N != 0)
+        sum++
+        current /= 10
+    } while (current != 0)
     return (sum)
 }
 
@@ -98,11 +98,13 @@ fun fib(n: Int): Int {
  * минимальное число k, которое делится и на m и на n без остатка
  */
 fun lcm(m: Int, n: Int): Int {
-    var k = 0
-    do {
-        k = k + 1
-    } while ((k % m != 0) || (k % n != 0))
-    return k
+    var a = n
+    var b = m
+    while (a != b) {
+        if (a > b) a -= b
+        else b -= a
+    }
+    return m * n / a
 }
 
 /**
@@ -140,11 +142,8 @@ fun maxDivisor(n: Int): Int {
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
 fun isCoPrime(m: Int, n: Int): Boolean {
-    var max = maxOf(m, n)
-    for (d in 2..max) {
-        if ((m % d == 0) && (n % d == 0)) {max = max + 1}
-    }
-return max == maxOf(m, n)
+    val x = (m * n / lcm(m, n))
+    return x == 1
 }
 
 /**
@@ -155,132 +154,136 @@ return max == maxOf(m, n)
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
 fun squareBetweenExists(m: Int, n: Int): Boolean {
-    var x = 0
     for (i in sqrt((m).toDouble()).toInt()..sqrt((n).toDouble()).toInt()) {
-        val I = i.toDouble()
-        if (sqr(I) in (m..n))
-        x = x + 1
+        if (sqr(i.toDouble()) in (m..n)) return true
     }
-    return x != 0
+    return false
 }
 
-
-/**
- * Средняя
- *
- * Для заданного x рассчитать с заданной точностью eps
- * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
- * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
- */
+    /**
+     * Средняя
+     *
+     * Для заданного x рассчитать с заданной точностью eps
+     * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
+     * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
+     */
 fun sin(x: Double, eps: Double): Double {
-    val l = x % (2 * PI)
-    return when {
-        l == PI / 2 -> 1.0
-        l == 3 * PI / 2 -> -1.0
-        else -> 0.0
-    }
+    var result = 0.0
+    val f = x % (2 * PI)
+    var n = 1.0
+    var m = 1.0
+    while (abs(pow(f, m)) / factorial(m.toInt()) >= eps) {
+        val z = pow(f, m) * n  / factorial(m.toInt())
+        m += 2
+        n *= -1
+        result += z
+        }
+    return result
 }
 
-/**
- * Средняя
- *
- * Для заданного x рассчитать с заданной точностью eps
- * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
- * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
- */
+    /**
+     * Средняя
+     *
+     * Для заданного x рассчитать с заданной точностью eps
+     * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
+     * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
+     */
 fun cos(x: Double, eps: Double): Double {
-    val l = x % (2 * PI)
-    return when {
-        l == PI / 2 -> 0.0
-        l == 3 * PI / 2 -> 0.0
-        l == PI -> -1.0
-        else -> 1.0
-    }
+    var result = 1.0
+    val f = x % (2 * PI)
+    var n = -1.0
+    var m = 2.0
+    while (abs(pow(f, m)) / factorial(m.toInt()) >= eps) {
+        val z = pow(f, m) * n  / factorial(m.toInt())
+        m += 2
+        n *= -1
+        result += z
+        }
+    return result
 }
+
+
 /**
- * Средняя
- *
- * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
- * Не использовать строки при решении задачи.
- */
+     * Средняя
+     *
+     * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
+     * Не использовать строки при решении задачи.
+     */
 fun revert(n: Int): Int {
     var revertNumber = 0
-    var N = n
+    var current = n
     do {
-        val x = N % 10
+        val x = current % 10
         revertNumber = revertNumber * 10 + x
-        N = N / 10
-    } while (N != 0)
+        current /= 10
+    } while (current != 0)
     return revertNumber
 }
 
-/**
- * Средняя
- *
- * Проверить, является ли заданное число n палиндромом:
- * первая цифра равна последней, вторая -- предпоследней и так далее.
- * 15751 -- палиндром, 3653 -- нет.
- */
+    /**
+     * Средняя
+     *
+     * Проверить, является ли заданное число n палиндромом:
+     * первая цифра равна последней, вторая -- предпоследней и так далее.
+     * 15751 -- палиндром, 3653 -- нет.
+     */
 fun isPalindrome(n: Int): Boolean {
-    var revertNumber = 0
-    var N = n
-    do {
-        val x = N % 10
-        revertNumber = revertNumber * 10 + x
-        N = N / 10
-    } while (N != 0)
-    return revertNumber == n
+    return revert(n) == n
 }
 
-/**
- * Средняя
- *
- * Для заданного числа n определить, содержит ли оно различающиеся цифры.
- * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
- */
+    /**
+     * Средняя
+     *
+     * Для заданного числа n определить, содержит ли оно различающиеся цифры.
+     * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
+     */
 fun hasDifferentDigits(n: Int): Boolean {
-    var N = n
+    var current = n
     var x = n % 10
     val count = n % 10
     do {
-        if (N % 10 != count) {x = N % 10}
-            N = N / 10
-    } while (N != 0)
+        if (current % 10 != count) {
+            x = current % 10
+            }
+        current /= 10
+    } while (current != 0)
     return x != count
 }
 
-/**
- * Сложная
- *
- * Найти n-ю цифру последовательности из квадратов целых чисел:
- * 149162536496481100121144...
- * Например, 2-я цифра равна 4, 7-я 5, 12-я 6.
- */
+    /**
+     * Сложная
+     *
+     * Найти n-ю цифру последовательности из квадратов целых чисел:
+     * 149162536496481100121144...
+     * Например, 2-я цифра равна 4, 7-я 5, 12-я 6.
+     */
 fun squareSequenceDigit(n: Int): Int {
     var x = 0
     var y = 0
     do {
-        x = x + 1
-        y = y + digitNumber(sqr(x.toDouble()).toInt())
+        x++
+        y += digitNumber(x * x)
     } while (y < n)
-    val r = sqr(x.toDouble()) / pow(10.0, (y - n).toDouble()) % 10.0
+    val r = x * x / pow(10.0, (y - n).toDouble()) % 10
     return r.toInt()
 }
 
-/**
- * Сложная
- *
- * Найти n-ю цифру последовательности из чисел Фибоначчи (см. функцию fib выше):
- * 1123581321345589144...
- * Например, 2-я цифра равна 1, 9-я 2, 14-я 5.
- */
+    /**
+     * Сложная
+     *
+     * Найти n-ю цифру последовательности из чисел Фибоначчи (см. функцию fib выше):
+     * 1123581321345589144...
+     * Например, 2-я цифра равна 1, 9-я 2, 14-я 5.
+     */
 fun fibSequenceDigit(n: Int): Int {
     var x = 0
     var y = 0
-    do {
-        x = x + 1
-        y = y + digitNumber(fib(x.toDouble().toInt()))
-    } while (y < n)
-    val r = fib(x.toDouble().toInt()) / pow(10.0, (y - n).toDouble()) % 10.0
-    return r.toInt()
+     do {
+         x++
+         y += digitNumber(fib(x))
+     } while (y < n)
+     val r = fib(x) / pow(10.0, (y - n).toDouble()) % 10
+     return r.toInt()
 }
+
+
